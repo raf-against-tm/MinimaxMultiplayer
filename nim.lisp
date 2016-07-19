6af5b13040da26a366ab120d924d676c29549686
Rafael Valle Morales <rafvall@gmail.com>
2016-07-19 19:44:31 +0200

    Completed maxn (minimax multiplayer) code. Added some modifications to NIM code in order to run with maxn algorithm.

--- nim.lisp
+++ nim.lisp
@@ -1,15 +1,32 @@
-;El siguiente codigo define los elementos necesarios para el juego NIM.
+;El siguiente codigo define los elementos necesarios para representar el juego NIM.
+
 ;El NIM consiste en un numero de fichas sobre un tablero de manera que cada jugador, en su turno, puede coger 1, 2 o 3 fichas.
 ; Pierde aquel jugador en cuyo turno no tenga fichas que coger.
 
+;Los diferentes estados del juego vendran determinados por el numero de fichas en el tablero, es decir, sera un numero natural.
+; El turno de cada jugador se indicara mediante un identificador.
+
 ;Definicion de variables globales
 
-(setf *estado-inicial* 4)
-(setf *movimientos* '(quitar-uno quitar-dos quitar-tres))
-(setf *minimo-valor* -1)
-(setf *maximo-valor* 1)
-(setf *max* 'max)
-(setf *min* 'min)
+(defvar *movimientos* '(quitar-uno quitar-dos quitar-tres))
+(defvar *minimo-valor* 0)
+(defvar *maximo-valor* 1)
+(defvar *max* 1)
+(defvar *min* 2)
+
+;Funciones de accesso a los elementos de un nodo del juego
+
+;Un nodo se constituye por una lista con un primer elemento como estado del juego y un segundo elemento que indica el turno del jugador.
+
+(defun estado (nodo)
+  "devuelve el estado del juego en el nodo dado, en este caso, el primer elemento de la lista"
+  (first nodo)
+)
+
+(defun turno (nodo)
+  "devuelve el identificador del jugador al que le toca mover, en este caso, el segundo elemento de la lista"
+  (second nodo)
+)
 
 ;Funciones necesarias para la representación del juego
 
@@ -19,8 +36,8 @@
 )
 
 (defun es-estado-ganador (estado turno jugador)
-  "identifica estado ganador para el jugador dado"
-  (if (and (es-estado-final estado) (eq turno jugador)) T NIL)
+  "identifica estado ganador para el jugador dado teniendo en cuenta el turno"
+  (if (and (es-estado-final estado) (not (eq turno jugador))) T NIL)
 )
 
 (defun aplica-movimiento (movimiento estado)
@@ -30,9 +47,9 @@
 
 (defun evaluacion-estatica (estado turno)
   "valora un nodo a partir del estado y el turno de movimiento en ese estado"
-  (cond ((es-estado-ganador estado turno 'max) 1)
-        ((es-estado-ganador estado turno 'min) -1)
-        (t 0)
+  (cond ((es-estado-ganador estado turno 1) (list 1 0))
+        ((es-estado-ganador estado turno 2) (list 0 1))
+        (t (list 0 0))
   )
 )
 
@@ -51,6 +68,18 @@
 (defun quitar-tres (estado)
   "quita tres fichas de la mesa"
   (if (>= estado 3) (- estado 3) 'no-aplicable)
+)
+
+;Funciones auxiliares
+
+(defun siguiente-turno (jugador)
+  "devuelve el identificador del jugador al que le toca mover en el siguiente turno"
+  (if (eq jugador 1) 2 1)
+)
+
+(defun construye-nodo (estado turno)
+  "devuelve un nodo del juego formado por el estado y el turno dados"
+  (list estado turno)
 )
   
     
